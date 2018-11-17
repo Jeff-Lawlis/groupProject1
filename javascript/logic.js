d53b7f747b15ee4717b4484cf9ab74023fed65f0
Jeff Lawlis <jeff.lawlis@yahoo.com>
2018-11-17 11:12:37 -0600

    swtiched to ajax retrieval

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,11 +1,27 @@
-const search = (event) => {
-    event.preventDefault();
-    const pokenumber = document.getElementById("pokeInput").value;
-    const baseURL = "https://pokeapi.co/api/v2/pokemon/";
-    $.get(baseURL + pokenumber, (pokemon) => {
-      document.getElementById("textName").innerHTML = pokemon.name;
-      document.getElementById("sprite").src = pokemon.sprites.front_default;
+// const search = (event) => {
+//     event.preventDefault();
+//     const pokenumber = document.getElementById("pokeInput").value;
+//     const baseURL = "https://pokeapi.co/api/v2/pokemon/";
+//     $.get(baseURL + pokenumber, (pokemon) => {
+//       document.getElementById("textName").innerHTML = pokemon.name;
+//       document.getElementById("sprite").src = pokemon.sprites.front_default;
+//     });
+//     console.log(pokenumber);
+//   }
+//   document.getElementById("pokeform").addEventListener('submit', search);
+
+  var pokeToSearch
+
+  $("#goButton").on("click", function(e){
+      e.preventDefault()
+      pokeToSearch = $("#pokeInput").val()
+    $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon/"+pokeToSearch+"/",
+        method: "GET"
+    }).then(function(response) {
+        console.log(response);
+        
+        $("#textName").text(response.name)
+        $("#sprite").attr("src", response.sprites.front_default)
     });
-    console.log(pokenumber);
-  }
-  document.getElementById("pokeform").addEventListener('submit', search);
+  })
