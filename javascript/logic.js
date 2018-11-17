c61fa8ce7cccdca7436c192a3709a1f32ab17979
unknown <wstegem@utexas.edu>
2018-11-17 15:28:23 -0600

    added substring search and search results display

--- javascript/logic.js
+++ javascript/logic.js
@@ -12,16 +12,71 @@
 
   var pokeToSearch
 
-  $("#goButton").on("click", function(e){
+//   $("#goButton").on("click", function(e){
+//       e.preventDefault()
+//       pokeToSearch = $("#pokeInput").val()
+//     $.ajax({
+//         url: "https://pokeapi.co/api/v2/pokemon/"+pokeToSearch+"/",
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response);
+        
+//         $("#textName").text(response.name)
+//         $("#sprite").attr("src", response.sprites.front_default)
+//     });
+//   })
+
+
+$("#goButton").on("click", function(e){
       e.preventDefault()
       pokeToSearch = $("#pokeInput").val()
     $.ajax({
-        url: "https://pokeapi.co/api/v2/pokemon/"+pokeToSearch+"/",
+        url: "https://pokeapi.co/api/v2/pokemon-species/",
+        method: "GET"
+    }).then(function(response) {
+
+        var searchList = document.createElement('div');
+            searchList.id = 'searchList';
+            searchList.className = 'info-scroll';
+
+        $('#pokeimage').empty()
+
+        for(i=0;i<response.count;i++){
+            if(response.results[i].name.toLowerCase().indexOf(pokeToSearch.toLowerCase()) !== -1){
+
+                console.log(response.results[i])
+
+                var listItem = document.createElement('p');
+                listItem.className = 'listItem';
+                listItem.id = 'listItem-'+i;
+                listItem.innerHTML = response.results[i].name
+                searchList.appendChild(listItem);
+                
+            }
+        }
+
+        $('#pokeimage').append(searchList);
+    });
+  })
+
+
+//This isn't firing for some reason
+$("#pokeimage").on('click', '.listItem', function(){
+    var pokeDisplay = this.innerHTML
+    console.log(pokeDisplay)
+    $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon/"+pokeDisplay+"/",
         method: "GET"
     }).then(function(response) {
         console.log(response);
         
         $("#textName").text(response.name)
-        $("#sprite").attr("src", response.sprites.front_default)
-    });
-  })
+
+        var sprite = document.createElement('img')
+        sprite.src = response.sprites.front_default
+        sprite.id = 'sprite-'+pokeDisplay
+        $('#pokeimage').empty()
+        $('#pokeimage').append(sprite);
+
+    })   
+})
